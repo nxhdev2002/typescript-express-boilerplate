e87ccf80e374b5e7e24a491e413d53ebdb680390
hoangnx <xuanhoangv7c@gmail.com>
2023-10-04 12:23:55 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'), { flags: 'a' }
+)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('combined', { stream: accessLogStream }))
 
 export default app
